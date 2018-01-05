295a6b3b53700c5306be3c70b41f36e98a972afa
Ramprasad A <ramprasad179@gmail.com>
2018-01-05 07:28:27 +0530

    Initial Commit
    Initial Project Commit.

---
+++ MCQ_Test/MCQ_Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MCQ_Test
+//
+//  Created by Ramprasad A on 05/01/18.
+//  Copyright © 2018 Ramprasad A. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
